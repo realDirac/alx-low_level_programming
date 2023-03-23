9bb74c09fb6b488faae86bdd9ebfaadad3fd7abf
Eugene <dir4c@outlook.com>
2023-03-23 01:05:26 +0000

    putchar file commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- *  * main - Entry point
- *   * this script is to print _putchar without using the standard lib
- *    * also does not make use of the functions printf and puts
- *     * Return: Always 0 (Success)
- *      */
+ * main - Entry point
+ * this script is to print _putchar without using the standard lib
+ * also does not make use of the functions printf and puts
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char str[] = "_putchar\n";
 	int i;
 
-	for (i=0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
