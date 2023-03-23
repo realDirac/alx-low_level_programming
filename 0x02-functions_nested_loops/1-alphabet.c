bb1072c3202b4d3ce8153d959f54483059de6dc1
Eugene <dir4c@outlook.com>
2023-03-23 02:12:20 +0000

    a function that prints lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,13 @@
  *Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+void print_alphabet(void)	/*function to print alphabets*/
 {
 	    char letter = 'a';
-	    while (letter <= 'z')
-	    {
-		    _putchar(letter);
-		    letter++;
-	    }
-	    _putchar('\n');
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
 }
