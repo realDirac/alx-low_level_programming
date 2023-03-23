e8fcde7054560834b8be906fefaa26f9a856b7eb
Eugene <dir4c@outlook.com>
2023-03-23 01:48:03 +0000

    a function that prints lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  * main - Entry point
+ *   * a function that prints the alphabet, in lowercase, followed by a new line
+ *    * Return: Always 0 (Success)
+ *     */
+
+void print_alphabet(void)
+{
+	    char letter = 'a';
+
+	    while (letter <= 'z')
+	    {
+		    _putchar(letter);
+		    letter++;
+	    }
+
+	    _putchar('\n');
+}
