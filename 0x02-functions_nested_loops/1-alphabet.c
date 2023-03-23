10cbc9c0786aba90f18d4b73b86a88be18a458d6
Eugene <dir4c@outlook.com>
2023-03-23 02:27:02 +0000

    a lowercase returning function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,8 @@
  *Return: Always 0 (Success)
  */
 
-void print_alphabet(void)	/*function to print alphabets*/
+void print_alphabet(void)
+	/*function to print alphabets*/
 {
 	    char letter = 'a';
 		while (letter <= 'z')
