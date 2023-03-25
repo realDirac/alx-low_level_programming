ee9659e9e3bc2451474272011f54c7c0a4751f20
Eugene <dir4c@outlook.com>
2023-03-25 05:10:34 +0000

    prints numbers from 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,15 @@
 
 void print_most_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c < 10; c++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		if (c != 2 && c != 4)
+		{
+			_putchar(c + '0');
+		}
 	}
+
 	_putchar('\n');
 }
