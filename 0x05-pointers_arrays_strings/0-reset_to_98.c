4cfeff1af7441775034d7accbe3389fe0bfeface
Eugene <dir4c@outlook.com>
2023-03-28 05:19:37 +0000

    takes a pointer to an int and updates the value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * resets_to_98 - updates the value of a variable back to 98
+ * reset_to_98 - updates the value of a variable back to 98
  * @n: variable to reset
  * Return: void
  */
