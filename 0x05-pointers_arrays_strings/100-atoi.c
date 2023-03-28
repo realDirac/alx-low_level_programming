e09dbd18a7afb782881f5b9b42b902923a393995
Eugene <dir4c@outlook.com>
2023-03-28 05:56:07 +0000

    a function that converts string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,15 +9,25 @@ int _atoi(char *s)
 {
 	int result = 0;
 	int sign = 1;
+	int digit_found = 0;
 
 	while (*s)
 	{
-		if (*s == '-')
-			sign = -sign;
-		if (*s >= '0' && *s <= '9')
+		if (*s == '-' || *s == '+')
+		{
+			if (digit_found)
+				break;
+			sign = (*s == '-')? -1 : 1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			digit_found = 1;
 			result = result * 10 + (*s - '0');
-		if (*s == '+' || (*s >= '0' && *s <= '9'))
+		}
+		else if (digit_found)
+		{
 			break;
+		}
 		s++;
 	}
 
