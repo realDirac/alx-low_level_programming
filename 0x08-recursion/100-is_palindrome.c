43ec8c8496cab249a1fa5561ef691a8c00e60cb8
Eugene <dir4c@outlook.com>
2023-04-05 08:55:08 +0000

    function that detects palindrom

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
-int find_strlen(char *s);
+int _strlen_recursion(char *s);
 int check_palindrome(char *s, int len, int index);
 int is_palindrome(char *s);
 
 /**
- * is_palindrome - function that works on string
- * check_palindrome - function checking palindrome
  * _strlen_recursion - function checking string length
  * @s: The string to be measured.
  * Return: The length of the string.
@@ -21,6 +19,16 @@ int _strlen_recursion(char *s)
 		return (1 + _strlen_recursion(s + 1));
 }
 
+/**
+ * check_palindrome - function checking palindrome
+ * _strlen_recursion - function checking string length
+ * @start: start of the string.
+ * @end: end of string
+ * @s: the string to measure
+ * Return: The length of the string.
+ */
+
+
 int check_palindrome(char *s, int start, int end)
 
 {
@@ -30,6 +38,15 @@ int check_palindrome(char *s, int start, int end)
 		return (0);
 	return (check_palindrome(s, start + 1, end - 1));
 }
+
+/**
+ * is_palindrome - function that works on string
+ * _strlen_recursion - string lenght
+ * check_palindrome - check palindrom
+ * @s: The string to be measured.
+ * Return: The length of the string.
+ */
+
 
 int is_palindrome(char *s)
 
